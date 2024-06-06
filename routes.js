6748ece287517037425c4e579129ffeb4c20c630
arachn0id887 <vivaanjha@gmail.com>
2024-06-06 22:28:59 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -9,13 +9,22 @@ router.get("/article", (req, res) => {
   res.render("articles");
 });
 router.get("/article/create", (req, res) => {
-  res.render("create");
+  res.render("create", { article: new article() });
 });
-router.post("/article/create", (req, res) => {
+
+router.post("/article/create", async (req, res) => {
   res.redirect("index");
   console.log(req.body);
+  try {
+    article = await article.save();
+    res.redirect(`/article/${article.id}`);
+  } catch (err) {
+    console.log(err);
+  }
 });
-router.get("/article/:id", (req, res) => {
+
+router.get("/article/:id", async (req, res) => {
+  await article.findById(req.params.id);
   res.render("get");
 });
 router.get("/article/:id/update", (req, res) => {
@@ -37,10 +46,17 @@ router.post("/article/:id/delete", (req, res) => {
 router.delete("/article/:id/delete", async (req, res) => {
   try {
     const { id } = req.params;
-    const art = await art.findByIdAndDelete(id);
-  } catch (error) {
+    await art.findByIdAndDelete(id);
+  } catch (err) {
     res.status(404);
+    console.log(err)
   }
+  res.redirect("index");
 });
+article.name = req.body.name;
+article.info = req.body.info;
+article.posted_by = req.body.posted_by;
+article.posted_at = new Date();
+article.last_updated_at = new Date();
 
 module.exports = router;
